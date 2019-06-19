409065b2515d717b4f7111fed28229fac82dc42b
kiranmayi20 <kiranmayi.mangali@walkingtree.tech>
2019-06-19 12:09:26 +0530

    inital commit

---
+++ app/model/Base.js
@@ -0,0 +1,7 @@
+Ext.define('customcomponent.model.Base', {
+    extend: 'Ext.data.Model',
+
+    schema: {
+        namespace: 'customcomponent.model'
+    }
+});
---
+++ app/model/Personnel.js
@@ -0,0 +1,7 @@
+Ext.define('customcomponent.model.Personnel', {
+    extend: 'customcomponent.model.Base',
+
+    fields: [
+        'name', 'email', 'phone'
+    ]
+});
